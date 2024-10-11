619f89bb9bd446f443cb621ecabb8d661316c85d
Mayank Gaur <mayankgaur1504@gmail.com>
2024-10-11 10:04:36 +0000

    hello

---
+++ DSA_QUESTIONS/arrays/level_0.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+// class Solution {
+// public:
+//     int maxArea(vector<int>& height) {
+
+        // brute force solution
+        //O(n2) ----> TLE
+        // (10^5)^2  
+        //we have to do in 10^8
+
+        // int maxWater = 0;
+
+        // for(int i = 0 ; i < height.size() ; i++)
+        // {
+        //     for(int j = i+1 ; j < height.size() ; j++)
+        //     {
+        //         int width = j-i;
+        //         int heightt = min(height[i] , height[j]);
+        //         int water = (width * heightt);
+        //         maxWater = max(maxWater , water);
+
+        //     }
+        // }
+
+        // return maxWater;
+
+
+        //optimal - O(n)
+
+
+//     int lp = 0;
+//     int rp = height.size() - 1;
+//     int maxWater = 0;
+
+//     while(lp < rp)
+//     {
+//          int width = rp-lp;
+//          int heightt = min(height[lp] , height[rp]);
+//          int water = width * heightt;
+//          maxWater = max(maxWater , water);
+
+//          height[lp] < height[rp] ? lp++ : rp--;
+//     }
+
+//     return maxWater;
+        
+//     }
+// };
+
+int main(){
+    cout<<"ek hi saas hai kbhi bhi ruk skti hai" <<endl;
+}
